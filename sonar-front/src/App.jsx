4064ba781ca6e8c50d08f9994d642433109a2ee7
jeonghaebin <dpdlzzz@naver.com>
2024-12-25 19:15:33 +0900

    Feat: 컨버스 추가

--- sonar-front/src/App.jsx
+++ sonar-front/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Splash from "./components/Splash";
 import Login from "./pages/Login";
+import DrawingCanvas from "./pages/DrawingCanvas";
 import Home from "./pages/Home";
 
 function App() {
@@ -12,10 +13,16 @@ function App() {
       {showSplash ? (
         <Splash onFinish={() => setShowSplash(false)} />
       ) : (
+        // <Router>
+        //   <Routes>
+        //     <Route path="/" element={<Login />} />
+        //     <Route path="/Home" element={<Home />} />
+        //   </Routes>
+        // </Router>
         <Router>
           <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/Home" element={<Home />} />
+            <Route path="/" element={<DrawingCanvas />} /> {/* 임시로 변경 */}
+            <Route path="/login" element={<Login />} />
           </Routes>
         </Router>
       )}
